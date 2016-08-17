d0bb752d903c2c360b6205fd6155824de759ffcc
IvanPre <ivan.prearo@hotmail.com>
2016-08-16 21:17:09 -0300

    Add files via upload
    Testar no linux

---
+++ TestaStr.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <levenshtein.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*********************************************
+Testa se uma string se encontra e um arquivo
+**********************************************/
+char* testacomando(char txt[512], char comando[512])
+{
+	FILE *file;
+	if((file=fopen(txt,"r"))==NULL)
+		printf("Arquivo não encontrado");
+	else
+	{
+		char comandos[1024], *cmndteste = (char *) malloc (1024);
+		int dif=99;
+		int a=0;
+		//Transfere um comando para "cmndteste"
+		for(int b=0; comandos[a]!='|'; b++, a++)
+		{
+			cmndteste[b]=comandos[a];
+			if((difstr(cmndteste,comando))<dif)
+				dif=difstr(cmndteste,comando);
+		}
+		fclose(file);
+		if(dif<2)
+			return cmndteste;
+		else
+		{//pergunta se foi aquilo que quis dizer
+			char perg[1024];
+			strcpy(perg,cmndteste);
+			strcat(perg, "? (s/n)*");
+			printf("\n\n                *Você quis dizer ");
+			puts(perg);
+			char resp;
+			do
+			{
+				scanf("%c",&resp);
+				if(resp=='s')//retorna o comando
+					return cmndteste;
+				else
+					if(resp=='n')
+					{
+						printf("*Ok*\n");
+						return '\0';
+					}
+			}while(resp!='s' && resp!='n');
+		}
+	}
+}
--- levenshtein.cpp
+++ levenshtein.cpp
@@ -1,27 +1,20 @@
-#include "levenshtein.h"
+#include <stdio.h>
 
-//Verifica a distância entre duas strings e mostra quntas operações são necessárias para transforar uma em outra
-int difstr(char str1[], char str2[])
+#define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
+
+int levenshtein(char *str1, char *str2, int len1, int len2) 
 {
-	//x e y são variáveis auxiliares
-	int x, y, s1len, s2len;
-    //s1len é o tamanho da string inserida
-	s1len = strlen(str1);
-	//s2len é o tamanho da string comparada
-    s2len = strlen(str2);
-    
-    int matrix[s2len+1][s1len+1];
-    matrix[0][0] = 0;
-    for (x = 1; x <= s2len; x++)           //Zera a coluna 0
-        matrix[x][0] = matrix[x-1][0] + 1;
-        
-    for (y = 1; y <= s1len; y++)           //Zera a linha 0
-        matrix[0][y] = matrix[0][y-1] + 1;
-        
-    //Compara as duas strings e calcula a diferença entre elas
-    for (x = 1; x <= s2len; x++)
-        for (y = 1; y <= s1len; y++)
-        	matrix[x][y] = (matrix[x-1][y] + 1, matrix[x][y-1] + 1, matrix[x-1][y-1] + (str1[y-1] == str2[x-1] ? 0 : 1));
-    
-    return matrix[s1len][s2len];
-}
+	int min = len1 > len2 ? len2 : len1 ;
+	int max = len1 > len2 ? len1 : len2 ;
+
+	if (min == 0) {
+		return max ;
+	} else {
+		int val[3] ;
+		val[0] = levenshtein(str1, str2, len1-1, len2) + 1 ;
+		val[1] = levenshtein(str1, str2, len1, len2-1) + 1 ;
+		int indcatorFunc = str1[len1-1] == str2[len2-1] ? 0 : 1 ;
+		val[2] = levenshtein(str1, str2, len1-1, len2-1) + indcatorFunc ;
+		return MIN3(val[0], val[1], val[2]) ;
+	}
+}		
